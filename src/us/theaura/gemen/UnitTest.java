05c541b193e14e327c9b4b6c1ac6b708b8f3d0b2
Gavin Lutz <Shortninja66@gmail.com>
2017-01-18 00:42:10 -0600

    Testing class

---
+++ src/us/theaura/gemen/UnitTest.java
@@ -0,0 +1,16 @@
+package us.theaura.gemen;
+
+/**
+ * Class with testing code.
+ * 
+ * @since 15 January 2017 4:39 PM
+ * @author Shortninja
+ */
+
+public class UnitTest {
+
+	public UnitTest() {
+		
+	}
+	
+}
